75e4a790aff66ae18dc8ff93c82b6a427f4cb49d
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-04-18 03:04:40 +0300

    Created structure of tools & figures. (No implementation).

---
+++ yakov.OOP.EnhancedPaint.Figures/Figures/FigureBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.OOP.EnhancedPaint.Tools
+{
+    public abstract class FigureBase: Tool
+    {
+
+
+
+    }
+}
---
+++ yakov.OOP.EnhancedPaint.Figures/Tool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.OOP.EnhancedPaint.Tools
+{
+    public enum ToolType
+    {
+        Pointer,
+        Pen,
+        Line,
+        Rect,
+        RoundedRect,
+        Square,
+        Ellipse,
+        Circle
+    }
+    public abstract class Tool
+    {
+        public ToolType ToolType { get; set; }
+    }
+}
