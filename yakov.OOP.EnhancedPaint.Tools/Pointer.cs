96725c0165dfda3ea65a5d88096c1cbb58767381
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-04-22 23:54:20 +0300

    Realized pointer tool & deleted pen tool.

--- yakov.OOP.EnhancedPaint.Tools/Pointer.cs
+++ yakov.OOP.EnhancedPaint.Tools/Pointer.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace yakov.OOP.EnhancedPaint.Tools
 {
     public class Pointer : Tool
     {
         public override ToolType ToolType { get; } = ToolType.Pointer;
+
+        public UIElement SelectItemOnCanvas(Canvas canvas, Point clickedPos)
+        {
+            for(int i = canvas.Children.Count - 1; i >= 0; i--)
+            {
+                if (canvas.Children[i].IsMouseOver)
+                    return canvas.Children[i];
+            }
+
+            return null;
+        }
     }
 }
