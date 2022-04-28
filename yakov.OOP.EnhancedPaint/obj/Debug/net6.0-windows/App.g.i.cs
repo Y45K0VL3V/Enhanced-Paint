20feed4a3957c9bff68e826cc24af35bb15a023e
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-04-28 14:08:30 +0300

    Some fixes & added serialization project.

--- yakov.OOP.EnhancedPaint/obj/Debug/net6.0-windows/App.g.i.cs
+++ yakov.OOP.EnhancedPaint/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace yakov.OOP.EnhancedPaint {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/yakov.OOP.EnhancedPaint;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/yakov.OOP.EnhancedPaint;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
