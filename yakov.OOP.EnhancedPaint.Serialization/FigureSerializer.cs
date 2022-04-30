7aede9f3920874c27ab0df466d378ac8d2a9a4cf
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-05-01 02:40:33 +0300

    Created structure of serialization.

---
+++ yakov.OOP.EnhancedPaint.Serialization/FigureSerializer.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yakov.OOP.EnhancedPaint.Figures;
+
+namespace yakov.OOP.EnhancedPaint.Serialization
+{
+    public class FigureSerializer : IFigureSerialize<FigureBase>
+    {
+        public List<FigureBase> Deserialize(string serializedFigures)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Serialize(List<FigureBase> figures)
+        {
+            return JsonConvert.SerializeObject(figures);
+        }
+    }
+}
---
+++ yakov.OOP.EnhancedPaint.Serialization/IFigureSerialize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.OOP.EnhancedPaint.Serialization
+{
+    public interface IFigureSerialize<T>
+    {
+        string Serialize(List<T> figures);
+        List<T> Deserialize(string serializedFigures);
+
+    }
+}
