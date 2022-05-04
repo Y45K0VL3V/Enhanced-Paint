e77c4a706325bd633f1a11a1ad07638410b65ce0
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-05-05 00:31:04 +0300

    Added crypt plugin.

---
+++ yakov.OOP.EnhancedPaint.CrypterPlugin/RabinCrypter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using yakov.OOP.EnhancedPaint.Plugins;
+
+namespace yakov.OOP.EnhancedPaint.CrypterPlugin
+{
+    public class RabinCrypter
+    {
+        public static byte[] Encrypt(BigInteger publicKeyN, BigInteger publicKeyB, byte[] inputData)
+        {
+            if (publicKeyN <= publicKeyB)
+                throw new Exception("Public key B must be lower, than N");
+
+            int maxEncryptedCapacity = publicKeyN.ToByteArray().Length;
+            byte[] encryptedData = new byte[maxEncryptedCapacity * inputData.Length];
+
+            for (int i = 0; i <= inputData.Length - 1; i++)
+            {
+                byte currByte = inputData[i];
+                var encryptedNum = currByte * (currByte + publicKeyB) % publicKeyN;
+                encryptedNum.ToByteArray().CopyTo(encryptedData, i * maxEncryptedCapacity);
+            }
+
+            return encryptedData;
+        }
+
+        private static long s_currEncryptedPos;
+
+        public static byte[] Decrypt(BigInteger privateKeyQ, BigInteger privateKeyP, BigInteger publicKeyB, byte[] encryptedData)
+        {
+            s_currEncryptedPos = 0;
+            BigInteger publicKeyN = privateKeyP * privateKeyQ;
+
+            int maxEncryptedCapacity = publicKeyN.ToByteArray().Length;
+            var decryptedBytes = new byte[encryptedData.Length / maxEncryptedCapacity];
+
+            for (long i = 0; i <= encryptedData.Length / maxEncryptedCapacity - 1; i++)
+            {
+                var currNumber = new BigInteger(GetNumberBytes(encryptedData, maxEncryptedCapacity));
+                decryptedBytes[i] = DecryptNumber(currNumber, privateKeyQ, privateKeyP, publicKeyN, publicKeyB);
+            }
+
+            return decryptedBytes;
+        }
+
+        private static byte DecryptNumber(BigInteger number, BigInteger privateKeyQ, BigInteger privateKeyP, BigInteger publicKeyN, BigInteger publicKeyB)
+        {
+            BigInteger discriminant = (publicKeyB * publicKeyB + 4 * number) % publicKeyN;
+
+            var mp = BigInteger.ModPow(discriminant, (privateKeyP + 1) / 4, privateKeyP);
+            var mq = BigInteger.ModPow(discriminant, (privateKeyQ + 1) / 4, privateKeyQ);
+
+            RabinHelpMath.ExtendedEuclidean(privateKeyP, privateKeyQ, out BigInteger yp, out BigInteger yq);
+
+            var dResults = new BigInteger[4]
+            {
+                    (yp * privateKeyP * mq + yq * privateKeyQ * mp) % publicKeyN,
+                    publicKeyN,
+                    (yp * privateKeyP * mq - yq * privateKeyQ * mp) % publicKeyN,
+                    publicKeyN
+            };
+            (dResults[1], dResults[3]) = (publicKeyN - dResults[0], publicKeyN - dResults[2]);
+
+            var mResults = new BigInteger[4];
+            for (int i = 0; i < 4; i++)
+            {
+                mResults[i] = ((dResults[i] - publicKeyB).IsEven) ? (dResults[i] - publicKeyB) / 2 % publicKeyN : (dResults[i] + publicKeyN - publicKeyB) / 2 % publicKeyN;
+            }
+
+            foreach (BigInteger currRes in mResults)
+                if (currRes >= 0 && currRes <= 255)
+                    return currRes.ToByteArray()[0];
+
+            return 0;
+        }
+
+        // For decrypt only.
+        private static byte[] GetNumberBytes(byte[] srcArray, int length)
+        {
+            var bytes = new byte[length];
+            long endPos = s_currEncryptedPos + length - 1;
+
+            try
+            {
+                while (s_currEncryptedPos <= endPos)
+                    // Works properly, if length stays the same for full array.
+                    bytes[s_currEncryptedPos % length] = srcArray[s_currEncryptedPos++];
+            }
+            catch { return new byte[length]; }
+
+            return bytes;
+        }
+
+    }
+}
---
+++ yakov.OOP.EnhancedPaint.CrypterPlugin/RabinHelpMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.OOP.EnhancedPaint.CrypterPlugin
+{
+    internal class RabinHelpMath
+    {
+        public static BigInteger ExtendedEuclidean(BigInteger a, BigInteger b, out BigInteger x, out BigInteger y)
+        {
+            if (b < a)
+            {
+                (b, a) = (a, b);
+            }
+
+            if (a == 0)
+            {
+                x = 0;
+                y = 1;
+                return b;
+            }
+
+            BigInteger gcd = ExtendedEuclidean(b % a, a, out x, out y);
+
+            BigInteger newY = x;
+            BigInteger newX = y - (b / a) * x;
+
+            x = newX;
+            y = newY;
+            return gcd;
+        }
+    }
+}
